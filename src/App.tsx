298d7cadfdc79b736c2f5e6d4780113bac986d9b
Matheus Teixeira <matheusbispo56@gmail.com>
2022-10-14 18:06:46 -0300

    Commit Inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+export function App() {
+
+  return (
+
+    <>
+      <h1 className=""> Testando</h1>
+
+      <button className="bg-cyan-500 font-medium px-2 rounded text-white hover:bg-violet-600">Enviar</button>
+    </>
+  )
+}
+
